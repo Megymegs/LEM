c542e3b7dcd6cd8430941274e8379e0cd431b811
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 16:26:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
     $( ".display-pam" ).click(function() {
         $("#pamphlet" ).toggle();
-        $('#page1').show();
+        $('#page1, #heading1').show();
     });
 
     $( "#pamphlet button[data-bs-dismiss='modal']" ).click(function() {
         $( "#pamphlet" ).toggle();
+    });
+
+    $( ".page1" ).click(function() {
+        $('#page1, #heading1').show();
+        $('#page2, #heading2').hide();
+        $('#page3, #heading3').hide();
+    });
+
+    $( ".page2" ).click(function() {
+        $('#page2, #heading2').show();
+        $('#page1, #heading1').hide();
+        $('#page3, #heading3').hide();
+    });
+    
+    $( ".page3" ).click(function() {
+        $('#page3, #heading3').show();
+        $('#page1, #heading1').hide();
+        $('#page2, #heading2').hide();
     });
 })
