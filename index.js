cf10ef3d3eddcbd29934f063be1eb2b0ca371a71
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 19:34:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     });
 
     $( ".page2" ).click(function() {
-        $('#page2, #heading2, .page1, .page3').show();
+        $('#page2, #heading2, .page1, .page3, #through-it-all img, #through-it-all h4, #through-it-all p, #oceans-where-feet-may-fail img, #oceans-where-feet-may-fail h4, #oceans-where-feet-may-fail p, #when-the-roll-is-called-up-yonder img, #when-the-roll-is-called-up-yonder h4, #when-the-roll-is-called-up-yonder p').show();
         $('#page1, #heading1, .page2, #page3, #heading3').hide();
         $('.hymes div').removeClass('col-11 mx-auto').addClass('col-md-4 col-10')
     });
@@ -31,18 +31,21 @@ $(document).ready(function () {
     $( "#hyme1, #through-it-all h3").click(function() {
         $('#page2, #heading2, .page1, .page3, #through-it-all, #through-it-all img, #through-it-all h4, #through-it-all p').show();
         $('#page1, .page2, #page3, #heading1, #heading3, #oceans-where-feet-may-fail img, #oceans-where-feet-may-fail h4, #oceans-where-feet-may-fail p, #when-the-roll-is-called-up-yonder img, #when-the-roll-is-called-up-yonder h4, #when-the-roll-is-called-up-yonder p').hide();
-        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10')
+        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10');
+        $('#oceans-where-feet-may-fail h3, #when-the-roll-is-called-up-yonder h3').css('font-size', '1.2em');
     });
     
     $( "#hyme2, #oceans-where-feet-may-fail h3").click(function() {
         $('#page2, #heading2, .page1, .page3, #oceans-where-feet-may-fail, #oceans-where-feet-may-fail img, #oceans-where-feet-may-fail h4, #oceans-where-feet-may-fail p').show();
         $('#page1, .page2, #page3, #heading1, #heading3, #through-it-all img, #through-it-all h4, #through-it-all p, #when-the-roll-is-called-up-yonder img, #when-the-roll-is-called-up-yonder h4, #when-the-roll-is-called-up-yonder p').hide();
-        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10')
+        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10');
+        $('#through-it-all h3, #when-the-roll-is-called-up-yonder h3').css('font-size', '1.2em');
     });
     
     $( "#hyme3, #when-the-roll-is-called-up-yonder h3").click(function() {
         $('#page2, #heading2, .page1, .page3, #when-the-roll-is-called-up-yonder, #when-the-roll-is-called-up-yonder img, #when-the-roll-is-called-up-yonder h4, #when-the-roll-is-called-up-yonder p').show();
         $('#page1, .page2, #page3, #heading1, #heading3, #through-it-all img, #through-it-all h4, #through-it-all p, #when-the-roll-is-called-up-yonder img, #oceans-where-feet-may-fail img, #oceans-where-feet-may-fail h4, #oceans-where-feet-may-fail p').hide();
-        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10')
+        $('.hymes div').addClass('col-11 mx-auto').removeClass('col-md-4 col-10');
+        $('#through-it-all h3, #oceans-where-feet-may-fail h3').css('font-size', '1.2em');
     });
 })
