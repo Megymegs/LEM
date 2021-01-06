bc81fae2d89e86bab3b9b8d7c64ab374a7761247
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 17:52:18 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $( ".display-pam" ).click(function() {
         $("#pamphlet" ).toggle();
         $('#page1, #heading1').show();
+        $('.page1').hide();
     });
 
     $( "#pamphlet button[data-bs-dismiss='modal']" ).click(function() {
@@ -12,20 +13,17 @@ $(document).ready(function () {
     });
 
     $( ".page1" ).click(function() {
-        $('#page1, #heading1').show();
-        $('#page2, #heading2').hide();
-        $('#page3, #heading3').hide();
+        $('#page1, #heading1, .page2, .page3').show();
+        $('.page1, #page2, #heading2, #page3, #heading3').hide();
     });
 
     $( ".page2" ).click(function() {
-        $('#page2, #heading2').show();
-        $('#page1, #heading1').hide();
-        $('#page3, #heading3').hide();
+        $('#page2, #heading2, .page1, .page3').show();
+        $('#page1, #heading1, .page2, #page3, #heading3').hide();
     });
     
     $( ".page3" ).click(function() {
-        $('#page3, #heading3').show();
-        $('#page1, #heading1').hide();
-        $('#page2, #heading2').hide();
+        $('#page3, #heading3, .page1, .page2').show();
+        $('.page3, #page1, #heading1, #page2, #heading2').hide();
     });
 })
