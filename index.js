418a728eebeb3e70e4d4e4cec422ebbb58d30e7a
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 15:10:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-$('.display-pam').on('click', function(){
-    if($('#pamphlet').css('display') == 'none') {
-        $('#pamphlet').css('display','block');
-    }
+$(document).ready(function () {
+    $('.display-pam').on('click', function(){
+        if($('#pamphlet').css('display') == 'none') {
+            $('#pamphlet').css('display','block');
+        }
+    });
 })
