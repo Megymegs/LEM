e2fb95d2e89802fe4791c2ab1fe8b72d7e4b643d
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 15:34:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $( ".display-pam" ).click(function() {
+        $("#pamphlet" ).toggle();
+        $('#page1').show();
+    });
+
+    $( "#pamphlet button[data-bs-dismiss='modal']" ).click(function() {
         $( "#pamphlet" ).toggle();
     });
 })
