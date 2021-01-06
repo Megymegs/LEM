a21c9266fb1971f961fc0d6641cbb7b5a30a09b7
Megan Meyer <33827558+Megymegs@users.noreply.github.com>
2021-01-06 20:39:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
     $( ".page2" ).click(function() {
         $('#page2, #heading2, .page1, .page3, #through-it-all img, #through-it-all h4, #through-it-all p, #oceans-where-feet-may-fail img, #oceans-where-feet-may-fail h4, #oceans-where-feet-may-fail p, #when-the-roll-is-called-up-yonder img, #when-the-roll-is-called-up-yonder h4, #when-the-roll-is-called-up-yonder p').show();
         $('#page1, #heading1, .page2, #page3, #heading3').hide();
-        $('.hymes div').removeClass('col-11 mx-auto').addClass('col-md-4 col-10')
+        $('.hymes div').removeClass('col-11').addClass('col-md-4 col-10')
     });
     
     $( ".page3" ).click(function() {
